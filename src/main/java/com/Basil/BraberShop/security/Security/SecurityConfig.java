42abda5d791aec0ad6f65f4f6e9051b37bd803ad
hussain.61 <husseinyousf505@gmail.com>
2022-03-16 18:08:17 +0200

    ddf

--- src/main/java/com/Basil/BraberShop/security/Security/SecurityConfig.java
+++ src/main/java/com/Basil/BraberShop/security/Security/SecurityConfig.java
@@ -19,10 +19,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import static com.Basil.BraberShop.enums.UserRole.VISITOR;
 
 @Configuration
+@CrossOrigin(origins = "*", maxAge = 3600, allowedHeaders={"x-auth-token", "x-requested-with", "x-xsrf-token"})
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -61,7 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 @Override
 protected void configure(HttpSecurity http) throws Exception {
     http
-            .csrf().disable()
+            .csrf().disable().cors().and()
 //       .csrf()
 //      .          .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
